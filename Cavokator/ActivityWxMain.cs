756609404622a3025d0974fbf3b605c0ec0373f8
Manuel Ortega <manuortega@gmail.com>
2017-11-29 21:23:22 +0100

    Drawer appearing

--- Cavokator/ActivityWxMain.cs
+++ Cavokator/ActivityWxMain.cs
@@ -1,23 +1,24 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace Cavokator
 {
     [Activity(Label = "Cavokator", MainLauncher = true, Icon = "@drawable/ic_appicon",
-         Theme = "@android:style/Theme.Material", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+         Theme = "@style/Theme.AppCompat.Light", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
 
-    public class ActivityWxMain : Activity
+    public class ActivityWxMain : AppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.wx_weather_main);
+            SetContentView(Resource.Layout.drawer_layout);
 
 
 
             //var newFragment = new WxMetarFragment();
             //var ft = FragmentManager.BeginTransaction();
-            //ft.Add(Resource.Id.flContent, newFragment);
+            //ft.Add(Resource.Id.weather_fragment_container, newFragment);
             //ft.Commit();
         }
     }
